13d460daf1a7cf533000a3c4533733dcc883bc9a
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-06-19 13:20:22 +0500

    feat: сделать компонент Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="page">
       <Header />
-      <p>hgfj</p>
+      <Main />
+
     </div>
   );
 }
