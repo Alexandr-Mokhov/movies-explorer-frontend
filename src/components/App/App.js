338b2d4ea21c9122bae3c5054307f5feee368702
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-06-21 14:55:01 +0500

    feat: сделать компонент Profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Header />
       {/* <Main /> */}
       {/* <Movies /> */}
-      <SavedMovies />
+      {/* <SavedMovies /> */}
+      <Profile />
     </div>
   );
 }
