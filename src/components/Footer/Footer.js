bf7aeb98c71f83f6c6fcc0baa74f2e97626cf687
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-06-25 12:26:52 +0500

    feat: сделать адаптив на 320рх

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,36 +6,36 @@ export default function Footer() {
 
   return (
     <>
-      {pathname === "/" || pathname === "/movies" || pathname === "/saved-movies" ? 
-      <footer className="footer">
-        <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-        <div className="footer__info">
-          <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
-          <ul className="footer__links">
-            <li className="footer__list">
-              <a
-                className="footer__link"
-                href="https://practicum.yandex.ru/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Яндекс.Практикум
-              </a>
-            </li>
-            <li className="footer__list">
-              <a
-                className="footer__link"
-                href="https://github.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github
-              </a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-      : ''}
+      {pathname === "/" || pathname === "/movies" || pathname === "/saved-movies" ?
+        <footer className="footer">
+          <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+          <div className="footer__info">
+            <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+            <ul className="footer__links">
+              <li className="footer__list">
+                <a
+                  className="footer__link"
+                  href="https://practicum.yandex.ru/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Яндекс.Практикум
+                </a>
+              </li>
+              <li className="footer__list">
+                <a
+                  className="footer__link"
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+        : ''}
     </>
   )
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,36 +6,36 @@ export default function Portfolio() {
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__item">
-          <p className="portfolio__item-text">Статичный сайт</p>
-          <a
+          <a className="portfolio__link"
             href="https://alexandr-mokhov.github.io/how-to-learn/"
             target="_blank"
             rel="noreferrer"
           >
-            <div className="portfolio__item-link" />
+            Статичный сайт
+            <div className="portfolio__link-arrow" />
           </a>
         </li>
         <li className="portfolio__item">
-          <p className="portfolio__item-text">Адаптивный сайт</p>
-          <a
+          <a className="portfolio__link"
             href="https://alexandr-mokhov.github.io/russian-travel/"
             target="_blank"
             rel="noreferrer"
           >
-            <div className="portfolio__item-link" />
+            Адаптивный сайт
+            <div className="portfolio__link-arrow" />
           </a>
         </li>
         <li className="portfolio__item">
-          <p className="portfolio__item-text">Одностраничное приложение</p>
-          <a
+          <a className="portfolio__link"
             href="https://alexandr-mokhov.github.io/react-mesto-auth/#/"
             target="_blank"
             rel="noreferrer"
           >
-            <div className="portfolio__item-link" />
+            Одностраничное приложение
+            <div className="portfolio__link-arrow" />
           </a>
         </li>
       </ul>
-    </section>
+    </section >
   )
 }
