9431c662e0905b7d72aa29dbe59c519f0884a977
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-06-26 21:20:50 +0500

    hotfix: исправить компонент Promo по макету, указать межбуквенные интервалы

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,17 +5,11 @@ export default function Promo() {
     <section className="promo">
       <div className="promo__logo" />
       <div className="promo__info">
-        <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+        <h1 className="promo__title">Учебный проект студента факультета Веб‑разработки.</h1>
         <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
         <ul className="promo__nav-tab">
           <li>
-            <a className="promo__button" href="#about-project">О проекте</a>
-          </li>
-          <li>
-            <a className="promo__button" href="#techs">Технологии</a>
-          </li>
-          <li>
-            <a className="promo__button" href="#about-me">Студент</a>
+            <a className="promo__button" href="#about-project">Узнать больше</a>
           </li>
         </ul>
       </div>
