d6cb8d89a1285616ad233f4cd611ad878b5a0a59
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-06-27 13:50:48 +0500

    hotfix: настроить центрирование страницы 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,11 +4,13 @@ import './NotFound.css';
 export default function NotFound() {
   return (
     <div className="not-found">
-      <div className="not-found__container-text">
-        <h2 className="not-found__title">404</h2>
-        <p className="not-found__subtitle">Страница не найдена</p>
+      <div className="not-found__container">
+        <div className="not-found__container-text">
+          <h2 className="not-found__title">404</h2>
+          <p className="not-found__subtitle">Страница не найдена</p>
+        </div>
+        <Link className="not-found__back" to="/">Назад</Link>
       </div>
-      <Link className="not-found__back" to="/">Назад</Link>
     </div>
   )
 }
