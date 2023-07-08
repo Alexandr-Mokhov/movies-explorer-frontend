ae5198e2f8a0a9c0363396e5e149e0e54f802037
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-07-08 17:17:23 +0500

    feat: установить допзависимость, поправить табуляцию

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -9,31 +9,31 @@ function setHeaders() {
 }
 
 function request(url, option) {
-	return fetch(`https://api.diploma.mokhov.nomoredomains.rocks${url}`, option).then(checkResponse);
+  return fetch(`https://api.diploma.mokhov.nomoredomains.rocks${url}`, option).then(checkResponse);
 }
 
 export const registerUser = ({ name, email, password }) => {
-	return request('/signup', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ name, email, password }),
-	})
+  return request('/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email, password }),
+  })
 }
 
 export const authorizeUser = ({ email, password }) => {
-	return request('/signin', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ email, password }),
-	})
+  return request('/signin', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  })
 }
 
 export const updateUserInfo = ({ name, email }) => {
-	return request('/users/me', {
-		method: 'PATCH',
-		headers: setHeaders(),
-		body: JSON.stringify({ name, email }),
-	})
+  return request('/users/me', {
+    method: 'PATCH',
+    headers: setHeaders(),
+    body: JSON.stringify({ name, email }),
+  })
 }
 
 export const checkToken = () => {
@@ -47,19 +47,19 @@ export function addStatusFavorite(movie) {
   return request(`/movies`, {
     method: 'POST',
     headers: setHeaders(),
-		body: JSON.stringify({
-			country: movie.country,
-			director: movie.director,
-			duration: movie.duration,
-			year: movie.year,
-			description: movie.description,
-			image: `https://api.nomoreparties.co${movie.image.url}`,
-			trailerLink: movie.trailerLink,
-			thumbnail: `https://api.nomoreparties.co${movie.image.url}`,
-			movieId: movie.id,
-			nameRU: movie.nameRU,
-			nameEN: movie.nameEN,
-		}),
+    body: JSON.stringify({
+      country: movie.country,
+      director: movie.director,
+      duration: movie.duration,
+      year: movie.year,
+      description: movie.description,
+      image: `https://api.nomoreparties.co${movie.image.url}`,
+      trailerLink: movie.trailerLink,
+      thumbnail: `https://api.nomoreparties.co${movie.image.url}`,
+      movieId: movie.id,
+      nameRU: movie.nameRU,
+      nameEN: movie.nameEN,
+    }),
   });
 }
 
