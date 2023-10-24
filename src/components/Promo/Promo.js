73e54e7fb693a1877e34939337256e7a32633fd5
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-10-24 17:48:26 +0500

    feat: изменить оформление секции Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,11 +6,13 @@ export default function Promo() {
       <div className="promo__container">
         <div className="promo__logo" />
         <div className="promo__info">
-          <h1 className="promo__title">Учебный проект студента факультета Веб‑разработки.</h1>
-          <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <h1 className="promo__title">Киноман</h1>
+          <p className="promo__subtitle_basic">Сервис по поиску фильмов*, просмотру трейлеров и добавлению их в избранное.</p>
+          <p className="promo__subtitle_basic">Зарегистрируйтесь чтобы начать просмотр.</p>
+          <p className="promo__subtitle_more">* Колличество фильмов ограничено Api (100 шт.)</p>
           <nav className="promo__nav-tab">
             <ul className="promo__nav-tab-list">
-              <li><a className="promo__button" href="#about-project">Узнать больше</a></li>
+              <li><a className="promo__button" href="#about-project">Подробнее</a></li>
             </ul>
           </nav>
         </div>
