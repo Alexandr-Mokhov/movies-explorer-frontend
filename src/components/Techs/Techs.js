15c6c982960c05ac74df87a28fbd48df2353ae07
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-10-24 18:13:40 +0500

    feat: изменить описание в секции techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -5,15 +5,17 @@ export default function Techs() {
     <section className="techs" id="techs">
       <div className="techs__container">
         <h2 className="techs__title">Технологии</h2>
-        <p className="techs__subtitle">7 технологий</p>
+        <p className="techs__subtitle">9 технологий</p>
         <p className="techs__subtitle-info">
-          На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+          В проекте применил технологии которые изучил на курсе разработки и освоил самостоятельно.
         </p>
         <ul className="techs__grid">
           <li className="techs__grid-item">HTML</li>
           <li className="techs__grid-item">CSS</li>
+          <li className="techs__grid-item">SASS</li>
           <li className="techs__grid-item">JS</li>
           <li className="techs__grid-item">React</li>
+          <li className="techs__grid-item">Redux</li>
           <li className="techs__grid-item">Git</li>
           <li className="techs__grid-item">Express.js</li>
           <li className="techs__grid-item">mongoDB</li>
