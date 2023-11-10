0c245ad743d4c904dfe000ef7cf14c611fff6e38
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-11-10 17:42:46 +0500

    feat: исправить ошибку в Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -9,7 +9,7 @@ export default function Promo() {
           <h1 className="promo__title">Киноман</h1>
           <p className="promo__subtitle_basic">Сервис по поиску фильмов*, просмотру трейлеров и добавлению их в избранное.</p>
           <p className="promo__subtitle_basic">Зарегистрируйтесь чтобы начать просмотр.</p>
-          <p className="promo__subtitle_more">* Колличество фильмов ограничено Api (100 шт.)</p>
+          <p className="promo__subtitle_more">* Количество фильмов ограничено Api (100 шт.)</p>
           <nav className="promo__nav-tab">
             <ul className="promo__nav-tab-list">
               <li><a className="promo__button" href="#about-project">Подробнее</a></li>
